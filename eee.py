bf96e4de7f91aef2ecda76d5a47a70bbc7de1e33
miguel <benedetty321@gmail.com>
2022-10-10 19:36:23 -0500

    paso

--- eee.py
+++ eee.py
@@ -1,1 +1,23 @@
-print("hola")
+print("usuario")
+q=input()
+print("password")
+w=input()
+
+print("ok registrado")
+
+print("inicir secion")
+
+print("username")
+r=input()
+print("password")
+t=input()
+
+if q==r:
+    print("username corresto")
+else:
+    print("username incorrecto")
+if w==t:
+    print("password corresto")
+else:
+    print("password incorresta")
+    
